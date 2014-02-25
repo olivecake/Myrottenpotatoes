0e10bbee2a2848530dc3644412607fcae037748c
Olivia Larson <olivialarson@gmail.com>
2014-02-24 22:16:27 -0400

    restart

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Myrottenpotatoes::Application.config.secret_token = '75851b33a9fa60934088b2b59b4b4ebf96a34e415323cc98d9635703949b0db9dc0d62ab3d817bfa727f5e3f645f5d635e65fa6741fa6e22ea2cea2c3a0ca907'
+Rottenpotatoes::Application.config.secret_token = '683d4dea01b4bc5f21a276ea8cedf23a4f26d44d8e8680a2c16fb0c10ee60daae78896b37cce798dc793dc270bc7c1325ab83b175c6b35aea8e2cb2d19a25f8f'
