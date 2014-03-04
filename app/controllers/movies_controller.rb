b0668c8187209d37af49f5b9489db9b8488e155f
Olivia Larson <larsono1@ri105-2-5lin.admsu.montclair.edu>
2014-03-04 17:02:44 -0500

    final product

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,23 +7,51 @@ class MoviesController < ApplicationController
   end
 
   def index
-    order_by = params[:order_by] || session[:order_by]
-    if params[:order_by]
-	session[:order_by] = order_by
-    elsif session[:order_by]
-	redirect_to :order_by => order_by
+    redirect = false
+    @all_ratings = Movie.all_ratings
+
+    ratings = params[:ratings] || session[:ratings]
+    if params[:ratings]
+	session[:ratings] = params[:ratings]
+    elsif session[:ratings]
+	redirect = true
+	params[:ratings] = session[:ratings]
+	session.delete(:ratings)
     end
 
-    if order_by == "title"
-	@movies = Movie.order("title ASC")
-        @title_class = "hilite"
-    elsif order_by == "release_date"
-	@movies = Movie.order("release_date ASC")
-	@release_date_class = "hilite"
-    else 
+    order_by = params[:order_by] || session[:order_by]
+    if params[:order_by]
+	session[:order_by] = params[:order_by]
+    elsif session[:order_by]
+	redirect = true
+	session.delete(:order_by)
+	params[:order_by] = session[:order_by]
+    end
+
+    if redirect
+	flash.keep
+	redirect_to movies_path(:order_by=> order_by, :ratings=> ratings)
+	return
+    end
+
+debugger 
+    if !order_by.nil? and !session[:ratings].nil?
+	@movies = Movie.find(:all, :order => "#{order_by} ASC", :conditions => {:rating => session[:ratings].keys})
+    elsif !order_by.nil? and session[:ratings].nil?
+	@movies = Movie.order("#{order_by} ASC")
+    elsif !session[:ratings].nil? and order_by.nil?
+	@movies = Movie.find(:all, :conditions => {:rating => session[:ratings].keys})
+    else
 	@movies = Movie.all
     end
+
+    if session[:order_by] == "title"
+	@title_class = "hilite"
+    elsif session[:order_by] == "release_date"
+	@release_date_class = "hilite"
+    end
   end
+ 
 
   def new
     # default: render 'new' template
